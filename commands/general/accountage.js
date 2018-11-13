6a01e1fdc953100aceaff1b7163f20edd6fd7d73
Wokki <iiro.seppanen@student.hamk.fi>
2018-11-13 16:07:13 +0200

    Display userage in days also

--- commands/general/accountage.js
+++ commands/general/accountage.js
@@ -2,29 +2,33 @@ const { Command } = require('discord.js-commando');
 const moment = require('moment');
 
 module.exports = class AccountAgeCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: 'accountage',
-            group: 'general',
-            memberName: 'accountage',
-            description: 'Get your Discord accounts creation date',
-            guildOnly: true,
-            clientPermissions: ['SEND_MESSAGES'],
-            args: [
-                {
-                    key: 'user',
-                    prompt: 'Whose account age are we checking?',
-                    type: 'string',
-                    default: ''
-                }
-            ]
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: 'accountage',
+      group: 'general',
+      memberName: 'accountage',
+      description: 'Get your Discord accounts creation date',
+      guildOnly: true,
+      clientPermissions: ['SEND_MESSAGES'],
+      args: [
+        {
+          key: 'user',
+          prompt: 'Whose account age are we checking?',
+          type: 'string',
+          default: ''
+        }
+      ]
+    });
+  }
 
-    async run(msg, { user }) {
-        if (user === "") user = msg.author;
-        else user = msg.mentions.users.first(1)[0];
+  async run(msg, { user }) {
+    if (user === "") user = msg.author;
+    else user = msg.mentions.users.first(1)[0];
 
-        msg.channel.send(`Account **${user.username}** was created at **${moment(user.createdAt).format('DD.MM.YYYY HH:mm:ss')}**`);
-    }
+    msg.channel.send(`Account **${user.username}** was created at **${moment(user.createdAt).format('DD.MM.YYYY HH:mm:ss')}** which was **${this.numberWithCommas(moment().diff(user.createdAt, 'days'))} days ago**.`);
+  }
+
+  numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 }
